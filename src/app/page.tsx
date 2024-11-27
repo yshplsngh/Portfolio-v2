4b8b6e6454cd42682c8d0b4283a4492030110a04
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-11-27 11:43:09 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,20 @@ export default function Page() {
           </BlurFade>
         </section>
 
+        <section id="skills">
+          <div className="flex min-h-0 flex-col gap-y-3">
+            <BlurFade delay={BLUR_FADE_DELAY * 9}>
+              <h2 className="text-xl font-bold">Skills</h2>
+            </BlurFade>
+            <div className="flex flex-wrap gap-1">
+              {DATA.skills.map((skill, id) => (
+                  <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                    <Badge key={skill}>{skill}</Badge>
+                  </BlurFade>
+              ))}
+            </div>
+          </div>
+        </section>
 
         <section id="projects">
           <div className="space-y-12 w-full py-12">
@@ -157,21 +171,6 @@ export default function Page() {
                   />
                 </BlurFade>
             ))}
-          </div>
-        </section>
-
-        <section id="skills">
-          <div className="flex min-h-0 flex-col gap-y-3">
-            <BlurFade delay={BLUR_FADE_DELAY * 9}>
-              <h2 className="text-xl font-bold">Skills</h2>
-            </BlurFade>
-            <div className="flex flex-wrap gap-1">
-              {DATA.skills.map((skill, id) => (
-                  <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
-                    <Badge key={skill}>{skill}</Badge>
-                  </BlurFade>
-              ))}
-            </div>
           </div>
         </section>
 
